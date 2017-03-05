b8d9f6cbc7fd3a927df72a2783fd29a6f021a705
Reynau <xavi.lopez.reynau@gmail.com>
2017-03-05 04:33:25 +0100

    Google maps works

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import {MapPage} from "../pages/map/map";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    MapPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    MapPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/map/map.ts
@@ -0,0 +1,31 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+declare let google;
+
+@Component({
+  selector: 'page-map',
+  templateUrl: 'map.html'
+})
+export class MapPage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap(){
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+}
